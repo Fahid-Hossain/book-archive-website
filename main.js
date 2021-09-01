00ccfec87a70eace800737e400a5fa162359995f
Fahid Hossain <fahidhossain2021@gmail.com>
2021-09-02 04:23:40 +0600

    error handling

--- main.js
+++ main.js
@@ -1,36 +1,65 @@
-const getBookName=() =>{
-    // console.log("clicked");
+
+//error handling 
+const errorDisplay = document.getElementById("errorDisplay");
+
+const getBookName = () => {
     const input_field = document.getElementById("input-field");
-    const inputValue = input_field.value;
-    console.log(inputValue);
+    let inputValue = input_field.value;
+    if (inputValue === "") {
+        errorDisplay.innerHTML = "Enter a book name to show result .."
+
+        //clear display Book result 
+        displayBooks.textContent = "";
+
+        // clear total result found
+        const totalResult = document.getElementById("totalResultFound");
+        totalResult.innerHTML = "";
+        return;
+    }
+    else {
+        const url = `https://openlibrary.org/search.json?q=${inputValue}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => getAllBooks(data))
+
+    }
+
     //clear input field
     input_field.value = "";
+    //clear error message
+    errorDisplay.innerHTML = "";
 
+    //clear display Book result 
+    displayBooks.textContent = "";
 
-    const url = `https://openlibrary.org/search.json?q=${inputValue}`;
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>getAllBooks(data))
+    // clear total result found
+    const totalResult = document.getElementById("totalResultFound");
+    totalResult.innerHTML = "";
+
 
 }
 
-const getAllBooks= (books)=>{
-    // console.log(books);
+const getAllBooks = (books) => {
+
+    //error handling (no result found) 
+    if (books.numFound === 0) {
+        errorDisplay.innerHTML = "no result found .."
+        return;
+    }
 
     // total result found display
     const displayBooks = document.getElementById("displayBooks");
     const totalResult = document.getElementById("totalResultFound");
-    totalResult.innerHTML=`
+    totalResult.innerHTML = `
     <i class="text-white"> total Book result found:  ${books.numFound}</i>
      
     `;
 
 
-    books.docs.forEach(book=>{
-        // console.log(book);
+    books.docs.forEach(book => {
         const div = document.createElement("div");
         div.classList.add("col");
-        div.innerHTML=`
+        div.innerHTML = `
         <div class="card h-100">
             <img class="fluid w-50 h-75 mx-auto mt-2" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="book_cover">
             <div class="card-body">
